605fd0a95cce0961f74bf32424757465115792dc
harsh <harsh.aayush.r1@gmail.com>
2020-05-14 14:09:29 +0530

    Designed home page

--- frontend/src/app/modules/after-login/home/home.component.ts
+++ frontend/src/app/modules/after-login/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from './shared.service';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  clickEventsubscription:Subscription;
 
-  constructor() { }
+  constructor(private sharedService:SharedService) {
+    this.clickEventsubscription=this.sharedService.scrolltoSection.subscribe(id=>{
+      console.log(id)
+      this.scrollToElement(id);
+      })
+   }
 
   ngOnInit() {
   }
-
+  scrollToElement(id): void {
+    var ele=document.getElementById(id);
+    ele.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 }
