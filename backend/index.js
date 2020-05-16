3a23258211380a7938d41838a80f04bc596e8925
akashdeep-19 <akasdeeps19@gmail.com>
2020-05-16 20:29:28 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-
+const socketsio = require('socket.io');
 
 require('./config/database_config');
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-require('./services/stream')(io);
-
 const tweets = require("./routes/tweets");
 const auth = require('./routes/auth');
 
 const PORT = 3000
-server.listen(PORT,()=>console.log("listening at", PORT));
-
-
+const server = app.listen(PORT,()=>console.log("listening at", PORT));
+const io = socketsio(server);
+require('./services/stream')(io);
 
 app.use(bodyParser.json());
 app.use(cors());
