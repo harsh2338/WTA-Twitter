446375730845b3e0a2754bd328089903de2df3af
harsh <harsh.aayush.r1@gmail.com>
2020-05-17 19:35:26 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,10 @@ const express = require("express");
 const app = express();
 const socketsio = require('socket.io');
 
-var createError = require('http-errors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-
 require('./config/database_config');
 
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const tweets = require("./routes/tweets");
 const auth = require('./routes/auth');
@@ -26,30 +21,3 @@ app.use(cors());
 app.use("/tweets", tweets);
 app.use("/auth", auth);
 app.get("/stream", (req, res) => { res.sendFile(__dirname + '/public/client.html') });
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    //res.render('error');
-});
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
-module.exports = app;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,20 +7,13 @@ const jwt_key = require("../config/jwt_key")
 
 const router = express.Router();
 
-router.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
-
 router.post("/signup", (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
     bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err)
             return res.status(500).json({ error: err });
 
         user_data = {
-            name: req.body.username,
+            name: req.body.name,
             email: req.body.email,
             password: hash
         };
