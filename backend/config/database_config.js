172e30f4d818aa9f96483c941f7adbc38e210217
harsh <harsh.aayush.r1@gmail.com>
2020-06-18 18:09:04 +0530

    made some elements responsive

--- backend/config/database_config.js
+++ backend/config/database_config.js
@@ -1,10 +1,6 @@
 const mongo = require("mongoose");
 require('dotenv').config()
-
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_ADMIN_PASSWORD}@cluster0-rue61.mongodb.net/test?retryWrites=true&w=majority`;
-
-// const uri = "mongodb+srv://akash1:123@cluster0-8zkv9.mongodb.net/test?retryWrites=true&w=majority"
-
-mongo.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true},()=> {
+mongo.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("connected to db");
 });
