538b4424d99832e17a8899d3015cb4a2d41feeec
harsh <harsh.aayush.r1@gmail.com>
2020-06-19 17:07:19 +0530

    Final commit

--- backend/services/sent_api.js
+++ backend/services/sent_api.js
@@ -1,19 +1,18 @@
-// npm install natural --save
-var natural = require('natural');
-natural.PorterStemmer.attach();
-var Analyzer = natural.SentimentAnalyzer;
-var stemmer = natural.PorterStemmer;
-var analyzer = new Analyzer("English", stemmer, "afinn");
+const sentiment = require('sentiment');
 
+const senti = new sentiment();
 
-const preprocess = (str)=>{
-	str = " "+str+" ";
-          return str.tokenizeAndStem();
-	}
+const preprocess = (str) => {
+    str = " " + str + " "
+    return str.replace(/ the | a | an | so /g, ' ');
+}
+
 const sent_analyzer = (text) => {
-    return analyser.getSentiment(preprocess(text));
-const tweets_analyzer = async (tweets)=>{
-    for(let twt of tweets){
+    return senti.analyze(preprocess(text));
+}
+
+const tweets_analyzer = async(tweets) => {
+    for (let twt of tweets) {
         twt['score'] = sent_analyzer(twt.full_text);
     }
 }
@@ -22,30 +21,3 @@ module.exports = {
     sent_analyzer,
     tweets_analyzer
 };
-
-
-// const sentiment = require('sentiment');
-
-// const senti = new sentiment();
-
-// const preprocess = (str)=>{
-//     str = " "+str+" "
-//     return str.replace(/ the | a | an | so /g,' ');
-// }
-
-// const sent_analyzer = (text) => {
-//     return senti.analyze(preprocess(text));
-// }
-
-// const tweets_analyzer = async (tweets)=>{
-//     for(let twt of tweets){
-//         twt['score'] = sent_analyzer(twt.full_text);
-//     }
-// }
-
-// module.exports = {
-//     sent_analyzer,
-//     tweets_analyzer
-// };
-
-
